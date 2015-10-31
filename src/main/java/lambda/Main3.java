0a8762c3e49c6d719bd6e911542fabf412095c25
Timo Schäfer <timo.schaefer@gmail.com>
2015-10-31 19:16:16 +0100

    auto-generated interface

---
+++ src/main/java/lambda/Main3.java
@@ -0,0 +1,15 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+
+		List<String> l = Arrays.asList("Hello", "World", "Print", "My", "List");
+		l.forEach(System.out::println);
+		
+	}
+
+}
